bd583c77073fdfe41fcb11db87e2321650add309
TomymyFr <36853313+TomymyFr@users.noreply.github.com>
2022-06-13 16:55:26 +0200

    Restaurant

--- app/src/main/java/com/tom/calculator/MainActivity.kt
+++ app/src/main/java/com/tom/calculator/MainActivity.kt
@@ -2,8 +2,12 @@ package com.tom.calculator
 
 import android.os.Bundle
 import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.check
 
 class MainActivity : AppCompatActivity() {
     var a: Int = 1000
@@ -11,12 +15,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("le nombre de var est " + a)
-    }
-    fun changeText(view: View) {
-        if (view.id == R.id.button1) {
-            montexte.text = "Bouton 1 cliqué"
-        } else if (view.id == R.id.button2) {
-            montexte.text = "Bouton 2 cliqué"
+
+
+        button.setOnClickListener {
+            var montantTotal: Int = 0
+            
+            // if checkbox1 is checked, add 13 to the total
+            if (checkBox1.isChecked) {
+                montantTotal += 13
+            }
+            if (checkBox2.isChecked) {
+                montantTotal += 5
+            }
+            if (checkBox3.isChecked) {
+                montantTotal += 17
+            }
+            if (checkBox4.isChecked) {
+                montantTotal += 1
+            }
+            textView3.visibility = View.VISIBLE
+            textView3.text = "Votre total est de " + montantTotal.toString() + "€"
+
         }
     }
 }
