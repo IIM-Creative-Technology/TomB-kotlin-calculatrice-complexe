e84f39b36eb8109bd7dcad2e2aa6b440837d639a
TomymyFr <36853313+TomymyFr@users.noreply.github.com>
2022-06-14 09:59:04 +0200

    Radio

--- app/src/main/java/com/tom/calculator/MainActivity.kt
+++ app/src/main/java/com/tom/calculator/MainActivity.kt
@@ -1,41 +1,33 @@
 package com.tom.calculator
 
-import android.os.Bundle
-import android.view.View
-import android.widget.CheckBox
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.check
 
 class MainActivity : AppCompatActivity() {
-    var a: Int = 1000
+
+    var radioVar: RadioGroup? = null
+    lateinit var buttonVar: Button
+    lateinit var radioButtonVar: RadioButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("le nombre de var est " + a)
+        radioVar = findViewById(R.id.radioGroup)
+        buttonVar = findViewById(R.id.button)
+        buttonVar.setOnClickListener {
 
-
-        button.setOnClickListener {
-            var montantTotal: Int = 0
-            
-            // if checkbox1 is checked, add 13 to the total
-            if (checkBox1.isChecked) {
-                montantTotal += 13
-            }
-            if (checkBox2.isChecked) {
-                montantTotal += 5
-            }
-            if (checkBox3.isChecked) {
-                montantTotal += 17
-            }
-            if (checkBox4.isChecked) {
-                montantTotal += 1
-            }
-            textView3.visibility = View.VISIBLE
-            textView3.text = "Votre total est de " + montantTotal.toString() + "€"
+            val selectedId: Int = radioVar!!.checkedRadioButtonId
+            Log.i("DEbug", selectedId.toString())
+            radioButtonVar = findViewById(selectedId)
+            Toast.makeText(baseContext,radioButtonVar.text.toString(),Toast.LENGTH_LONG).show()
 
         }
     }
 }
+
